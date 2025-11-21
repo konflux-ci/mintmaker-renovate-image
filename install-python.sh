996c2cf3613aa8a85d16a3e946760f377e563f9d
staticf0x <staticf0x@pm.me>
2025-11-21 12:14:24 +0100

    Hardcode support for Python 3.9

--- install-python.sh
+++ install-python.sh
@@ -12,7 +12,12 @@ fi
 # Get the latest release tag (in format YYYYMMDD) and use that to filter all release assets
 # to find one matching cpython-{version}-{arch}-{configuration}.tar.gz, since the patch version
 # is not known to this script, i.e. we request "3.13", but we don't know that "3.13.7" is the latest
-RELEASE_TAG=$(curl -s -L -H "Accept: application/json" https://github.com/astral-sh/python-build-standalone/releases/latest | jq -r .tag_name)
+if [ "$PYTHON_VERSION" = "3.9" ]; then
+    RELEASE_TAG="20251028"
+else
+    RELEASE_TAG=$(curl -s -L -H "Accept: application/json" https://github.com/astral-sh/python-build-standalone/releases/latest | jq -r .tag_name)
+fi
+
 DOWNLOAD_URL=$(curl -s -L -H "Accept: application/json" "https://api.github.com/repos/astral-sh/python-build-standalone/releases/tags/$RELEASE_TAG" | jq ".assets.[]|select(.name | startswith(\"cpython-$PYTHON_VERSION\"))|select(.name | endswith(\"$ARCH-$CONFIGURATION.tar.gz\"))" | jq -r .browser_download_url)
 
 # Download the archive and extract into ~/python{version}
